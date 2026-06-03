465fda9db3c42cab95a733b1aed29232944506d4
alisherdadaxajaev-cloud <alisherdadaxajaev@gmail.com>
2026-06-03 08:13:20 +0500

    Add: 7) Global ro‘yxatga qo‘shish: log [alisherdadaxajaev-cloud]

---
+++ lms_kod_topshiriqlari/Funktsiyada_scope__local_va_global_o_zgaruvchilar/7__Global_ro_yxatga_qo_shish__log.py
@@ -0,0 +1,19 @@
+logs = []
+
+
+def add_log(msg):
+    logs.append(msg)
+    
+    
+n = int(input())
+
+
+for _ in range(n):
+    msg = input()
+    add_log(msg)
+    
+    
+print(len(logs))
+
+for item in logs:
+    print(item)
