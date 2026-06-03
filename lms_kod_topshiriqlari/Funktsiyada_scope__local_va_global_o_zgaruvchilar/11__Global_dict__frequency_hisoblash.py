e517b19a47915487ffa08c050facfb0d1710788a
alisherdadaxajaev-cloud <alisherdadaxajaev@gmail.com>
2026-06-03 08:13:30 +0500

    Add: 11) Global dict: frequency hisoblash [alisherdadaxajaev-cloud]

---
+++ lms_kod_topshiriqlari/Funktsiyada_scope__local_va_global_o_zgaruvchilar/11__Global_dict__frequency_hisoblash.py
@@ -0,0 +1,14 @@
+freq = {}
+
+def add_word(w):
+    global freq
+    w = w.lower()
+    freq[w] = freq.get(w, 0) + 1
+    
+n = int(input())
+
+for _ in range(n):
+    add_word(input().strip())
+    
+for word in sorted(freq):
+    print(word, freq[word])
